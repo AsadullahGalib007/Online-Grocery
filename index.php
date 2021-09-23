60eb0fcdd052a12ed61c233f0f9f20bcc69ef94f
AsadullahGalib007 <abrgalib@gmail.com>
2021-09-23 16:45:52 +0600

    Tried new style

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
     <!-- fOOD MEnu Section Starts Here -->
     <section class="food-menu">
         <div class="container">
-            <h2 class="text-center">Food Menu</h2>
+            <h2 class="text-center">Products</h2>
 
             <?php 
             
