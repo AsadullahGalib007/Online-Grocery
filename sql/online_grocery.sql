ef0acb06c9c838a74f739f8720720fcc2e3e6fdd
AsadullahGalib007 <abrgalib@gmail.com>
2021-10-04 15:44:18 +0600

    updated Database so far

--- sql/online_grocery.sql
+++ sql/online_grocery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 23, 2021 at 12:43 PM
+-- Generation Time: Oct 04, 2021 at 11:42 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -27,8 +27,6 @@ USE `online_grocery`;
 
 --
 -- Table structure for table `tbl_admin`
---
--- Creation: Sep 15, 2021 at 07:20 AM
 --
 
 CREATE TABLE `tbl_admin` (
@@ -57,8 +55,6 @@ INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
 --
 -- Table structure for table `tbl_category`
 --
--- Creation: Sep 15, 2021 at 07:20 AM
---
 
 CREATE TABLE `tbl_category` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -84,14 +80,13 @@ INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) V
 (25, 'Coffee', 'Food-Category-1004.jpg', 'Yes', 'Yes'),
 (26, 'MoMo', 'Food_Category_77.jpg', 'Yes', 'Yes'),
 (28, 'Pasta', 'Food-Category--8485.jpg', 'No', 'Yes'),
-(29, 'Jhal Muri', 'Food-Category-1003.jpg', 'No', 'No');
+(29, 'Jhal Muri', 'Food-Category-1003.jpg', 'No', 'No'),
+(38, 'nothing2', '', 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_food`
---
--- Creation: Sep 16, 2021 at 07:52 AM
 --
 
 CREATE TABLE `tbl_food` (
@@ -121,14 +116,14 @@ INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `ca
 (7, 'Mixed Pizza', 'Pizza with chicken, Ham, Buff, Mushroom and Vegetables', '10.00', 'Food-Name-7833.jpg', 4, 'Yes', 'Yes'),
 (9, 'Cake', 'Burst of sweetness. Must Try the RED VELVET.', '19.00', 'Food-Name-3224.jpg', 9, 'Yes', 'Yes'),
 (13, 'Pasta', 'Best pasta in town', '7.00', 'Food-Name-1003.jpg', 28, 'No', 'Yes'),
-(15, 'Dalgona Coffee', 'Try this icy, fresh, chocolaty dalgona coffee', '5.00', 'Food-Name-1004.jpg', 25, 'Yes', 'Yes');
+(15, 'Dalgona Coffee', 'Try this icy, fresh, chocolaty dalgona coffee', '5.00', 'Food-Name-1004.jpg', 25, 'Yes', 'Yes'),
+(21, 'nothing2', 'fesdfsdfsda', '34222.00', '', 38, 'Yes', 'Yes'),
+(22, 'nothing2', 'dsfasdfsdfsd', '11223343.00', 'Food-Name-1063.jpg', 38, 'Yes', 'Yes');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_order`
---
--- Creation: Sep 15, 2021 at 07:20 AM
 --
 
 CREATE TABLE `tbl_order` (
@@ -142,7 +137,8 @@ CREATE TABLE `tbl_order` (
   `customer_name` varchar(150) NOT NULL,
   `customer_contact` varchar(20) NOT NULL,
   `customer_email` varchar(150) NOT NULL,
-  `customer_address` varchar(255) NOT NULL
+  `customer_address` varchar(255) NOT NULL,
+  `f_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -153,17 +149,18 @@ CREATE TABLE `tbl_order` (
 -- Dumping data for table `tbl_order`
 --
 
-INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
-(1, 'Sadeko Momo', '6.00', 3, '18.00', '2020-11-30 03:49:48', 'Delivered', 'Bradley Farrell', '+1 (576) 504-4657', 'zuhafiq@mailinator.com', 'Duis aliqua Qui lor'),
-(2, 'Best Burger', '4.00', 4, '16.00', '2020-11-30 03:52:43', 'Delivered', 'Kelly Dillard', '+1 (908) 914-3106', 'fexekihor@mailinator.com', 'Incidunt ipsum ad d'),
-(3, 'Mixed Pizza', '10.00', 2, '20.00', '2020-11-30 04:07:17', 'Delivered', 'Jana Bush', '+1 (562) 101-2028', 'tydujy@mailinator.com', 'Minima iure ducimus');
+INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`, `f_id`) VALUES
+(1, 'Sadeko Momo', '6.00', 3, '18.00', '2020-11-30 03:49:48', 'Delivered', 'Bradley Farrell', '+1 (576) 504-4657', 'zuhafiq@mailinator.com', 'Duis aliqua Qui lor', 0),
+(2, 'Best Burger', '4.00', 4, '16.00', '2020-11-30 03:52:43', 'Delivered', 'Kelly Dillard', '+1 (908) 914-3106', 'fexekihor@mailinator.com', 'Incidunt ipsum ad d', 3),
+(3, 'Mixed Pizza', '10.00', 2, '20.00', '2020-11-30 04:07:17', 'Delivered', 'Jana Bush', '+1 (562) 101-2028', 'tydujy@mailinator.com', 'Minima iure ducimus', 0),
+(4, 'Cake', '19.00', 1, '19.00', '2021-10-04 14:34:59', 'On Delivery', 'Galib', '01701080021', 'abrgalib@gmail.com', 'asdfsdfsadfsdafd', 9),
+(5, 'Dalgona Coffee', '5.00', 1, '5.00', '2021-10-04 14:41:40', 'On Delivery', 'Asadullah', '1234567891011', 'asadullahgalib007@gmail.com', 'dsfsdfsadfsdafwerasdf', 15),
+(6, 'Dalgona Coffee', '5.00', 3, '15.00', '2021-10-04 14:49:56', 'Delivered', 'Oishe', '01778799990', 'oishe5949@gmail.com', 'Bogura', 15);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_vendor`
---
--- Creation: Sep 15, 2021 at 07:27 AM
 --
 
 CREATE TABLE `tbl_vendor` (
@@ -190,8 +187,6 @@ INSERT INTO `tbl_vendor` (`id`, `full_name`, `username`, `password`) VALUES
 
 --
 -- Table structure for table `tbl_v_c`
---
--- Creation: Sep 18, 2021 at 01:25 PM
 --
 
 CREATE TABLE `tbl_v_c` (
@@ -225,8 +220,6 @@ INSERT INTO `tbl_v_c` (`v_id`, `c_id`) VALUES
 --
 -- Table structure for table `tbl_v_f`
 --
--- Creation: Sep 18, 2021 at 01:28 PM
---
 
 CREATE TABLE `tbl_v_f` (
   `v_id` int(10) UNSIGNED DEFAULT NULL,
@@ -258,8 +251,6 @@ INSERT INTO `tbl_v_f` (`v_id`, `f_id`) VALUES
 
 --
 -- Table structure for table `tbl_v_o`
---
--- Creation: Sep 18, 2021 at 01:30 PM
 --
 
 CREATE TABLE `tbl_v_o` (
@@ -348,19 +339,19 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_vendor`
